2018c4219518033b486c04c26a11a829d9f01501
bo <bbollinger1@cub.uca.edu>
2018-03-14 23:38:31 -0500

    added Move class

---
+++ Gomoku/gomoku/Move.java
@@ -0,0 +1,46 @@
+package gomoku;
+
+public class Move 
+{
+	// private data members
+	private int row;
+	private int column;
+	
+	// default constructor
+	public Move() 
+	{
+		super();
+		this.row = 0;
+		this.column = 0;
+	}
+
+	// parameterized constructor
+	public Move(int row, int column) 
+	{
+		super();
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return "Move [row=" + row + ", column=" + column + "]";
+	}
+	
+}
