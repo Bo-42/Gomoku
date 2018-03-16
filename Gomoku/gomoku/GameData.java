2b2bf16124f10ebc49c3872b8f8a44a8c2ae1698
bo <bbollinger1@cub.uca.edu>
2018-03-15 20:45:16 -0500

    added... 	checkMove() 	addStone() 	checkWin()
    	methods to GameData class

--- Gomoku/gomoku/GameData.java
+++ Gomoku/gomoku/GameData.java
@@ -16,22 +16,50 @@ public class GameData implements Serializable
 	}
 	
 	// parameterized constructor
-	public GameData(Stone[][] board) 
+	public GameData( Stone[][] board ) 
 	{
 		super();
 		this.board = board;
 	}
 
-	public Stone[][] getBoard() {
+	public Stone[][] getBoard() 
+	{
 		return board;
 	}
 
-	public void setBoard(Stone[][] board) {
+	public void setBoard( Stone[][] board ) 
+	{
 		this.board = board;
+	}
+	
+	public boolean checkMove( Move move )
+	{	// returns 1 (true) if game board grid location from move (row, column) contains a stone
+		// returns 0 (false) if game board grid location from move (row, column) is empty
+		
+		if ( board[move.getRow()][move.getColumn()] == null)
+			return false;
+		else
+			return true;
+	}
+	
+	public void addStone( Move move, Stone stone )
+	{
+		this.board[ move.getRow() ][ move.getColumn() ].setColor( stone.getColor() );
+	}
+	
+	public boolean checkWin( Move move , Stone stone)
+	{	// returns a 1 (true) if a move creates a winning condition
+		// returns a 0 (false) if a move does not create a winning condition
+		boolean toReturn = false;
+		
+		//	To-Do
+		
+		return toReturn;
 	}
 
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		return "GameData [board=" + Arrays.toString(board) + "]";
 	}
 	
