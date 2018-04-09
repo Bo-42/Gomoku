8a8abe4b990f6b13efbd0046422e7a9ec5fe0230
wenjingli0612 <wenjinglicrystal@gmail.com>
2018-04-08 22:34:54 -0500

    add gamedata type

--- Gomoku/gomoku/ChatClient.java
+++ Gomoku/gomoku/ChatClient.java
@@ -7,7 +7,7 @@ public class ChatClient extends AbstractClient
   // Private data fields for storing the GUI controllers.
   private LoginControl loginControl;
   private CreateAccountControl createAccountControl;
-
+  private GameplayControl gpc;
   // Setters for the GUI controllers.
   public void setLoginControl(LoginControl loginControl)
   {
@@ -17,7 +17,10 @@ public class ChatClient extends AbstractClient
   {
     this.createAccountControl = createAccountControl;
   }
-
+  public void setGameplayControl(GameplayControl gpc)
+  {
+    this.gpc= gpc;
+  }
   // Constructor for initializing the client with default settings.
   public ChatClient()
   {
@@ -27,32 +30,42 @@ public class ChatClient extends AbstractClient
   // Method that handles messages from the server.
   public void handleMessageFromServer(Object arg0)
   {
-    // If we received a String, figure out what this event is.
-    if (arg0 instanceof String)
-    {
-      // Get the text of the message.
-      String message = (String)arg0;
-      
-      // If we successfully logged in, tell the login controller.
-      if (message.equals("Success:Login"))
-      {
-        loginControl.loginSuccess();
-      }
-      else if (message.equals("Failure:Login"))
-      {
-    	  loginControl.displayError("Failure:Login");
-      }
-      
-      // If we successfully created an account, tell the create account controller.
-      else if (message.equals("Success:Register"))
-      {
-        createAccountControl.createAccountSuccess();
-      }
-      else if (message.equals("Failure:Register"))
-      {
-    	  createAccountControl.displayError("Failure:Register");
-      }
-    }
+	  if (arg0 instanceof String)
+	    {
+	      // Get the text of the message.
+	      String message = (String)arg0;
+	      
+	      // If we successfully logged in, tell the login controller.
+	      if (message.equals("Success:Login"))
+	      {
+	        loginControl.loginSuccess();
+	      }
+	      else if (message.equals("Failure:Login"))
+	      {
+	    	  loginControl.displayError("Failure:Login");
+	      }
+	      
+	      // If we successfully created an account, tell the create account controller.
+	      else if (message.equals("Success:Register"))
+	      {
+	        createAccountControl.createAccountSuccess();
+	      }
+	      else if (message.equals("Failure:Register"))
+	      {
+	    	  createAccountControl.displayError("Failure:Register");
+	      }
+	      else if (message.substring(0, 6).equals("Winner:"))
+	     {
+	    	 gpc.dispalylabel(message);
+	      }
+	    }
+	    else if (arg0 instanceof  GameData)
+	    {//get GameData object
+	    	GameData gd= (GameData)arg0;
+	    	
+    	
+    	
+	    }
   
     
   }  
