39d9cf5b23755c0f1b89ef8caf02b4c4ed4f10b0
Fox <Fox@FOX>
2018-04-26 09:20:26 -0500

    Fixed commandline arguments for ClientGUI

--- Gomoku/gomoku/ClientGUI.java
+++ Gomoku/gomoku/ClientGUI.java
@@ -72,10 +72,11 @@ public class ClientGUI extends JFrame
 	// Main function that creates the client GUI when the program is started.
 	public static void main(String[] args)
 	{
-		if (args == null)
+		if (args.length != 2)
 		{
 			new ClientGUI("localhost", 8300);
-		} else
+		}
+		else
 		{
 			new ClientGUI(args[0], Integer.parseInt(args[1]));
 		}
